40311aa1cffb9fb3662b2e08957978eb553f5a7c
Eojin <140517360+GeomeunByeol@users.noreply.github.com>
2025-09-09 17:16:48 +0900

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,8 +1,6 @@
-python ./run/generate.py --quant "True" \
-    --huggingface-token "YOUR_HUGGINGFACE_TOKEN" \
+python ./run/generate.py --huggingface-token "YOUR_HUGGINGFACE_TOKEN" \
     --model-type "llama" \
     --model-id "meta-llama/Llama-3.1-8B-Instruct" \
-    --seed "$seed" \
     --dataset-dir "./dataset/ko" \
     --prompt-type "I" \
     --q-lang "en" \
