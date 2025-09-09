a16fe7f780ce309cf23cdaf995a81a3c1da4846e
Eojin <140517360+GeomeunByeol@users.noreply.github.com>
2025-09-09 17:13:44 +0900

    Add files via upload

---
+++ example.sh
@@ -0,0 +1,10 @@
+python ./run/generate.py --quant "True" \
+    --huggingface-token "YOUR_HUGGINGFACE_TOKEN" \
+    --model-type "llama" \
+    --model-id "meta-llama/Llama-3.1-8B-Instruct" \
+    --seed "$seed" \
+    --dataset-dir "../dataset/ko" \
+    --prompt-type "I" \
+    --q-lang "en" \
+    --i-lang "en" \
+    --t-lang "en"
